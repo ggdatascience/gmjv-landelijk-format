d85f482042e18d6d1f6f86999b642fe42fccddc3
Pieter Seinen <pieterseinen@gmail.com>
2024-10-23 14:16:21 +0200

    regiocode moet ook meegegeven worden in bulk uitdraai

--- maak_rapport_per_gemeente.R
+++ maak_rapport_per_gemeente.R
@@ -2,6 +2,10 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(dplyr)
 
+
+#Hier regiocode invoeren
+regiocode <- 23
+
 #vector met numerieke waarden gemeentecodes in regio
 gemeentecodes_in_regio = c(2,3,4,5,6) #codes nepgemeenten voorbeeldrapportage
 
@@ -12,7 +16,9 @@ for(gemeentecode in gemeentecodes_in_regio){
     input = "voorbeeld_rapportage.qmd", # Wijzig dit naar "Rapportage.qmd" als je deze voor meerdere gemeentes wil uitdraaien
     output_format = "html",
     output_file = glue::glue("gemeenterapport_{gemeentecode}.html"),
-    execute_params = list(gemeentecode = gemeentecode)
+    execute_params = list(
+      gemeentecode = gemeentecode,
+      regiocode = regiocode)
     )
   
 }
